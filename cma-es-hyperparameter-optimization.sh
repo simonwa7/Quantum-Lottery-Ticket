110e139f73548ccc03013f5e0827de0b3be5fe48
simonwa7 <william.andrew.simon@gmail.com>
2022-11-21 22:25:25 -0500

    Fixing filename in bash script

--- cma-es-hyperparameter-optimization.sh
+++ cma-es-hyperparameter-optimization.sh
@@ -7,11 +7,11 @@
 #SBATCH --mem=10000
 module load anaconda/3
 source activate /cluster/tufts/lovelab/wsimon02/condaenv/qlt
-python3 run_j1j2_vqe_qlt.py 5 5 16 & 
-python3 run_j1j2_vqe_qlt.py 5 5 32 & 
-python3 run_j1j2_vqe_qlt.py 5 5 64 & 
-python3 run_j1j2_vqe_qlt.py 5 5 128 & 
-python3 run_j1j2_vqe_qlt.py 5 5 256 & 
-python3 run_j1j2_vqe_qlt.py 5 5 512 & 
-python3 run_j1j2_vqe_qlt.py 5 5 1024 
+python3 cma-es-hyperparameter-optimization.py 5 5 16 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 32 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 64 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 128 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 256 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 512 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 1024 
 

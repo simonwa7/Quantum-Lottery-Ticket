035005e65f1e4bfc3855ca99b9522ad15f0b263d
simonwa7 <william.andrew.simon@gmail.com>
2022-11-21 23:51:27 -0500

    Fixing offsets

--- cma-es-hyperparameter-optimization.sh
+++ cma-es-hyperparameter-optimization.sh
@@ -7,11 +7,11 @@
 #SBATCH --mem=10000
 module load anaconda/3
 source activate /cluster/tufts/lovelab/wsimon02/condaenv/qlt
+python3 cma-es-hyperparameter-optimization.py 5 5 3 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 4 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 8 & 
 python3 cma-es-hyperparameter-optimization.py 5 5 16 & 
 python3 cma-es-hyperparameter-optimization.py 5 5 32 & 
 python3 cma-es-hyperparameter-optimization.py 5 5 64 & 
-python3 cma-es-hyperparameter-optimization.py 5 5 128 & 
-python3 cma-es-hyperparameter-optimization.py 5 5 256 & 
-python3 cma-es-hyperparameter-optimization.py 5 5 512 & 
-python3 cma-es-hyperparameter-optimization.py 5 5 1024 
+python3 cma-es-hyperparameter-optimization.py 5 5 128 
 

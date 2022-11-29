a5918325813a382bf88d4a2928d5bef155acf6cd
simonwa7 <william.andrew.simon@gmail.com>
2022-11-29 11:15:34 -0500

    Moving cluster scripts back

---
+++ cma-es-hyperparameter-optimization.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J j1j2-vqe-cma-es-hyperparameterization
+#SBATCH -p batch
+#SBATCH --time=7-00:00:00
+#SBATCH -n 1
+#SBATCH -c 8
+#SBATCH --mem=10000
+module load anaconda/3
+source activate /cluster/tufts/lovelab/wsimon02/condaenv/qlt
+python3 cma-es-hyperparameter-optimization.py 5 5 3 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 4 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 8 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 16 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 32 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 64 & 
+python3 cma-es-hyperparameter-optimization.py 5 5 128 
+

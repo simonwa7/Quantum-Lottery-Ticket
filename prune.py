b11e629ab13d2698a340c864ad7d1ee7b88dbf78
simonwa7 <william.andrew.simon@gmail.com>
2023-01-02 12:11:02 -0500

    Adding function to prune by percentage of parameters

--- prune.py
+++ prune.py
@@ -10,6 +10,37 @@ def get_parameter_indices_to_be_pruned(parameters, cutoff, period):
     ]
 
 
+def get_parameter_indices_to_be_pruned_using_percentage(
+    parameters, percentage_to_prune, period
+):
+    number_of_parameters_to_prune = np.ceil(len(parameters) * percentage_to_prune)
+
+    magnitudes_from_nearest_zero = []
+    for parameter in parameters:
+        remainder = parameter % period
+        magnitudes_from_nearest_zero.append(
+            min(
+                abs(0.0 - remainder),
+                abs(period - remainder),
+            )
+        )
+
+    pruned_indices = []
+    while len(pruned_indices) < number_of_parameters_to_prune:
+        min_magnitude = np.inf
+        min_index = None
+        for index, magnitude in enumerate(magnitudes_from_nearest_zero):
+            if index in pruned_indices:
+                continue
+
+            if magnitude < min_magnitude:
+                min_magnitude = magnitude
+                min_index = index
+        pruned_indices.append(min_index)
+
+    return sorted(pruned_indices)
+
+
 def get_pruned_parameters(unpruned_parameters, pruned_indices):
     pruned_parameters = unpruned_parameters
     for pruned_index in pruned_indices[::-1]:

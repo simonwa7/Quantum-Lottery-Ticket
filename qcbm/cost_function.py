312beaaef3b4707e5d485126c57507c89f068f22
simonwa7 <william.andrew.simon@gmail.com>
2023-01-14 18:35:38 -0500

    Adding data and adding weight decay to qcbm

--- qcbm/cost_function.py
+++ qcbm/cost_function.py
@@ -1,4 +1,4 @@
-from prune import get_padded_parameters
+from prune import get_padded_parameters, calculate_parameter_weight_bias
 from zquantum.qcbm.ansatz import QCBMAnsatz
 from zquantum.qcbm.cost_function import create_QCBM_cost_function
 from qeqiskit.simulator import QiskitSimulator
@@ -16,6 +16,8 @@ def get_pruned_qcbm_cost_function(
     pruned_indices,
     use_wandb=True,
     n_samples=None,
+    parameter_period=2 * np.pi,
+    weight_decay=0,
 ):
     number_of_qubits = int(np.log2(len(target_distribution)))
     target_distribution = create_bitstring_distribution_from_probability_distribution(
@@ -39,21 +41,32 @@ def get_pruned_qcbm_cost_function(
     )
 
     min_cost = np.inf
+    min_cnll = np.inf
 
     def pruned_cost_function(parameters):
-        nonlocal min_cost
+        nonlocal min_cost, min_cnll
 
         parameters = get_padded_parameters(parameters, pruned_indices)
 
-        cost = unpruned_cost_function(parameters)
+        cnll = unpruned_cost_function(parameters)
+        parameter_bias = calculate_parameter_weight_bias(
+            parameters, parameter_period, weight_decay
+        )
+        cost = cnll + parameter_bias
         min_cost = min(min_cost, cost)
+        min_cnll = min(min_cnll, cnll)
         if use_wandb:
             log_dict = {
-                "Clipped Negative Log Likelihood": cost,
-                "Minimum Clipped Negative Log Likelihood": min_cost,
-                "Offset Clipped Negative Log Likelihood": cost - minimum_possible_cnll,
-                "Minimum Offset Clipped Negative Log Likelihood": min_cost
+                "Minimum Offset Cost": min_cost - minimum_possible_cnll,
+                "Minimum Offset Clipped Negative Log Likelihood": min_cnll
                 - minimum_possible_cnll,
+                "Offset Cost": cost - minimum_possible_cnll,
+                "Offset Clipped Negative Log Likelihood": cnll - minimum_possible_cnll,
+                "Cost": cost,
+                "Minimum Cost": min_cost,
+                "Parameter Weight Bias": parameter_bias,
+                "Clipped Negative Log Likelihood": cnll,
+                "Minimum Clipped Negative Log Likelihood": min_cnll,
                 "Number of Circuits Run": backend.number_of_circuits_run,
             }
             wandb.log(log_dict)
